3213714659d74edbba27a13a94a8559cf8b8b4d7
asipachev <asipachev@autodor-207-032.vrn.local>
2014-01-31 13:46:56 +0400

    Initial commit

--- JavaDB/src/javadb/MySQLConnection.java
+++ JavaDB/src/javadb/MySQLConnection.java
@@ -10,6 +10,39 @@ package javadb;
  *
  * @author asipachev
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class MySQLConnection {
     
+    public void connect(){
+        try{
+        Class.forName("com.mysql.jdbc.Driver");
+        pln("Driver loading success");
+        
+        String url=("jdbc:mysql://195.98.74.37/vrndor");
+        //String url=("jdbc:mysql://bora.beget.ru");
+        String name = "datauser";
+        String password = "!QAX2wsz";
+        
+        try{
+            Connection con = DriverManager.getConnection(url,name,password);
+            pln("Connected");
+            con.close();;
+            pln("disonnected");
+                        
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        
+        }catch (ClassNotFoundException e){
+            pln(e);
+        }
+        
+    }
+    public void pln (Object s){
+        System.out.println(s);
+    }
 }
