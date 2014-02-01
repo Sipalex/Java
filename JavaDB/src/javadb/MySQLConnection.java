2f512d62061b4a4b8b98554d566642dffffbfcd5
asipachev <asipachev@autodor-207-032.vrn.local>
2014-02-01 14:11:06 +0400

    Initial commit

--- JavaDB/src/javadb/MySQLConnection.java
+++ JavaDB/src/javadb/MySQLConnection.java
@@ -13,29 +13,58 @@ package javadb;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class MySQLConnection {
     
-    public void connect(){
+    private final static String createDatabaseQuery="CREATE DATABASE bookstore CHARACTER SET utf8 COLLATE utf8_general_ci";
+    
+    
+    public void connect(String parturl, String user, String password){
+        Connection connection = null;
+        Statement statement = null;
+        
+        
+        
         try{
         Class.forName("com.mysql.jdbc.Driver");
         pln("Driver loading success");
         
-        String url=("jdbc:mysql://195.98.74.37/vrndor");
+        //String url=("jdbc:mysql://195.98.74.37/vrndor");
+        String url=("jdbc:mysql:"+parturl);
         //String url=("jdbc:mysql://bora.beget.ru");
-        String name = "datauser";
-        String password = "!QAX2wsz";
+               
         
-        try{
-            Connection con = DriverManager.getConnection(url,name,password);
-            pln("Connected");
-            con.close();;
-            pln("disonnected");
-                        
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+            try{
+                connection = DriverManager.getConnection(url,user,password);
+                pln("Connected");
+                statement = connection.createStatement();
+                
+                /*connection.close();;
+                pln("disonnected");*/
+                statement.executeUpdate(createDatabaseQuery);
+                
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }finally{
+                if(statement!=null){
+                  try{
+                      statement.close();
+                  } catch (SQLException e){
+                      e.printStackTrace();
+                      
+                  }
+                }
+                if (connection != null){
+                    try{
+                        connection.close();
+                    }catch (SQLException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
         
         }catch (ClassNotFoundException e){
             pln(e);
